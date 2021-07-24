5259d867b5cf14ad04cd48eeddffcf39b5831197
Arun Pariyar <arun.pariyar@gmail.com>
2021-07-24 08:29:28 +0200

    Pointer handsOn Completed, you are doing great

---
+++ intro_to_pointer/handsOn/handsOn1.go
@@ -0,0 +1,15 @@
+// Create a value and assign it to a variable.
+// Print the address of that value.
+package main
+
+import "fmt"
+
+func main(){
+	name:= "Chip Rogers"
+
+	//address of the value
+	fmt.Println(&name)
+}
+
+
+
